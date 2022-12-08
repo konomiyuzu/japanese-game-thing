958690598efa9d8831026a519c803080f542682b
konomiyu <autophukla@gmail.com>
2022-12-08 14:04:44 +0700

    completely rewritten game.js and other systems, got rid of points and accuracy system, bug testing required

--- js/resultsHandler.js
+++ js/resultsHandler.js
@@ -1,27 +1,24 @@
 
 class ResultsHandler {
     static elements;
-
+    static initiated = false;
+    static results = [];
+    static showDetailedResults = false
     static data = {
-        results: [],
-        showDetailedResults: false,
         score: 0,
         scoreMax: 0,
-        accuracyString: "0.00%",
-        points: 0,
-        timeString: "00:00:000",
-        initiated: false
+        timeString: "00:00:000"
     }
 
     static init(elements) {
         this.elements = elements;
         this.elements.showDetailedButton.addEventListener("click", this.toggleDetailedResults.bind(this));
-        this.data.initiated = true;
+        this.initiated = true;
     }
 
 
     static #createResult(questionText, correctText, chosenText, answerIsCorrect) {
-        let result = this.elements.template.content.cloneNode(true);
+        let result = this.elements.templateElement.content.cloneNode(true);
 
         //cloneNode() returns a document fragment,
         //this gets the node from the docfrag
@@ -46,55 +43,53 @@ class ResultsHandler {
     }
 
     static addResult(questionText, correctText, chosenText, answerIsCorrect) {
-        if (!this.data.initiated) throw new Error("ResultHandler not initialized");
+        if (!this.initiated) throw new Error("ResultHandler not initialized");
 
         let result = this.#createResult(questionText, correctText, chosenText, answerIsCorrect);
         this.elements.resultsContainer.appendChild(result);
-        this.data.results.push(result);
+        this.results.push(result);
     }
 
     static removeResult(index) {
-        if (!this.data.initiated) throw new Error("ResultHandler not initialized");
+        if (!this.initiated) throw new Error("ResultHandler not initialized");
 
-        let result = this.data.results[index];
+        let result = this.results[index];
 
         if (result == null) throw new Error(`result does not exist at index: ${index}`)
-        this.data.results.splice(index, 1);
+        this.results.splice(index, 1);
         result.remove();
     }
 
     static showResultsScreen() {
-        if (!this.data.initiated) throw new Error("ResultHandler not initialized");
+        if (!this.initiated) throw new Error("ResultHandler not initialized");
 
         this.updateText()
         this.elements.resultsScreenElement.setAttribute("style", "display:flex;")
     }
 
     static hideResultsScreen() {
-        if (!this.data.initiated) throw new Error("ResultHandler not initialized");
+        if (!this.initiated) throw new Error("ResultHandler not initialized");
 
         this.elements.resultsScreenElement.setAttribute("style", "display:none;")
     }
 
     static updateText() {
-        if (!this.data.initiated) throw new Error("ResultHandler not initialized");
+        if (!this.initiated) throw new Error("ResultHandler not initialized");
 
         this.elements.resultsTimeText.innerHTML = `time: ${this.data.timeString}`
         this.elements.resultsScoreText.innerHTML = `score: ${this.data.score}/${this.data.scoreMax}`
-        this.elements.resultsAccuracyText.innerHTML = `accuracy: ${this.data.accuracyString}`
-        this.elements.resultsPointsText.innerHTML = `points: ${this.data.points}`
     }
 
     static toggleDetailedResults() {
-        if (!this.data.initiated) throw new Error("ResultHandler not initialized");
+        if (!this.initiated) throw new Error("ResultHandler not initialized");
 
-        if (this.data.showDetailedResults) {
+        if (this.showDetailedResults) {
             this.elements.resultsContainer.setAttribute("style", "display:none;")
-            this.data.showDetailedResults = false;
+            this.showDetailedResults = false;
             this.elements.showDetailedButton.innerHTML = "show detailed results";
         } else {
             this.elements.resultsContainer.setAttribute("style", "display:flex;")
-            this.data.showDetailedResults = true;
+            this.showDetailedResults = true;
             this.elements.showDetailedButton.innerHTML = "hide detailed results";
         }
     }
@@ -106,8 +101,6 @@ const resultsContainerElement = document.getElementById("resultsContainer");
 const showDetailedButtonElement = document.getElementById("showResults");
 const resultsTimeText = document.getElementById("resultsTime");
 const resultsScoreText = document.getElementById("resultsScore");
-const resultsAccuracyText = document.getElementById("resultsAccuracy")
-const resultsPointsText = document.getElementById("resultsPoints")
 
 const resultHandlerElements = {
     templateElement: resultTemplate,
@@ -116,7 +109,5 @@ const resultHandlerElements = {
     showDetailedButton: showDetailedButtonElement,
     resultsTimeText: resultsTimeText,
     resultsScoreText: resultsScoreText,
-    resultsAccuracyText: resultsAccuracyText,
-    resultsPointsText: resultsPointsText
 }
 ResultsHandler.init(resultHandlerElements)
