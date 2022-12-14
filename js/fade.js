0b9b1535e9214e076fb9cb3c0363975ed1777c7c
konomiyu <autophukla@gmail.com>
2022-12-14 09:13:35 +0700

    updated fade to 60fps and made it throw an error if fade is in progress

--- js/fade.js
+++ js/fade.js
@@ -1,42 +1,54 @@
-class Fade{
-    
-    static fadeIn(timeSeconds,element,displayType = "block"){
+class Fade {
+
+    static fadeIn(timeSeconds, element, displayType = "block") {
+        //idk if using the classList to store this info is best practice or whatever it works
+        if (element.classList.contains("fading")) throw new Error("element already fading in/out");
+        element.classList.add("fading");
+
         let opacity = 0;
-        //interval is ran 20 times/second
-        const deltaOpacity = 1/(20 * timeSeconds);
+
+        //interval is ran 60 times/second
+        const deltaOpacity = 1 / (60 * timeSeconds);
 
         element.style.opacity = 0;
         element.style.display = displayType;
-    
 
-        let interval = setInterval(()=>{
-    
+
+        let interval = setInterval(() => {
+
             element.style.opacity = opacity;
             opacity += deltaOpacity;
-    
-            if(opacity >= 1){
+
+            if (opacity >= 1) {
                 clearInterval(interval);
+                element.classList.remove("fading");
+
             }
-        },50)
+        }, 1000 / 60)
     }
-    
-    static fadeOut(timeSeconds,element){
+
+    static fadeOut(timeSeconds, element) {
+        //idk if using the classList to store this info is best practice or whatever it works
+        if (element.classList.contains("fading")) throw new Error("element already fading in/out");
+        element.classList.add("fading");
+
         let opacity = 1;
-        //interval is ran 20 times/second
-        const deltaOpacity = 1/(20 * timeSeconds);
+        //interval is ran 60 times/second
+        const deltaOpacity = 1 / (60 * timeSeconds);
 
         element.style.opacity = 1;
-    
-        let interval = setInterval(()=>{
-    
+
+        let interval = setInterval(() => {
+
             element.style.opacity = opacity;
             opacity -= deltaOpacity;
-    
-            if(opacity <= 0){
+
+            if (opacity <= 0) {
                 clearInterval(interval);
                 element.style.display = "none";
+                element.classList.remove("fading");
             }
-        },50)
+        }, 1000 / 60)
     }
 }
 
